4d674fd5b42915101a9f2c9e2900600582137745
Deddy Silalahi <walmen_cobain@yahoo.com>
2016-07-22 18:04:32 +0700

    Add Cart, still error

---
+++ app/src/main/java/com/example/multimatics/myshoppingmall/API/db/CartHelper.java
@@ -0,0 +1,7 @@
+package com.example.multimatics.myshoppingmall.API.db;
+
+/**
+ * Created by Multimatics on 22/07/2016.
+ */
+public class CartHelper {
+}
---
+++ app/src/main/java/com/example/multimatics/myshoppingmall/CustomOnItemSelectedListener.java
@@ -0,0 +1,7 @@
+package com.example.multimatics.myshoppingmall;
+
+/**
+ * Created by Multimatics on 22/07/2016.
+ */
+public class CustomOnItemSelectedListener {
+}
